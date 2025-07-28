d1f898a8fcc965ce3bb8db5ec8c065056959116e
Your Name <you@example.com>
2025-07-28 13:55:25 +0530

    ups

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+const x = 22;
+console.log(x);
+const y = "Gaurav";
+console.log(y);
---
+++ index.ts
@@ -0,0 +1,5 @@
+const x : number = 22;
+console.log(x);
+
+const y: string = "Gaurav";
+console.log(y);
